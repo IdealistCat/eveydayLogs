9f2fbda0c2460ff3053520bd2db0236a97a045df
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-08 15:06:07 -0500

    this will actually replace the text

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 import { SITE_VERSION } from "./GLOBAL.js"
 
 var htmlSiteVer = document.getElementById('version_text');
-htmlSiteVer.innerTEXT = SITE_VERSION;
+htmlSiteVer.innerHTML = SITE_VERSION;
